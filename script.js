02fd2abd1658e84b8b5740cc413f8098773b448e
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2022-08-08 14:11:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//BootStrap Validation system
+//https://getbootstrap.com/docs/5.2/forms/validation/
+(() => {
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
